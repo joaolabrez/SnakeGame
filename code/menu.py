ab3ec9d1f6ebb7c48fd7a1a5a02c77a19012a7c7
João <joaolabresdm@gmail.com>
2025-08-31 14:56:04 -0300

    Removing some comments!

--- code/menu.py
+++ code/menu.py
@@ -1,4 +1,3 @@
-# menu.py
 import pygame
 import sys
 from pathlib import Path
@@ -6,9 +5,9 @@ from const import *
 from drawing import exibir_mensagem
 from game_loop import rodar_jogo
 
-# --- Lógica para encontrar a pasta de assets ---
 CAMINHO_RAIZ = Path(__file__).parent.parent
 CAMINHO_ASSETS = CAMINHO_RAIZ / "assets"
+
 
 def musica_menu():
     try:
@@ -18,18 +17,18 @@ def musica_menu():
     except pygame.error as e:
         print(f"Erro ao carregar musica_menu.wav: {e}")
 
-# --- SUA NOVA FUNÇÃO DE MÚSICA DE FINAL ---
+
 def musica_ending():
     try:
-        # Carrega o seu novo arquivo de música
         pygame.mixer.music.load(CAMINHO_ASSETS / 'musica_end.wav')
         pygame.mixer.music.play(-1)
         pygame.mixer.music.set_volume(0.3)
     except pygame.error as e:
         print(f"Erro ao carregar musica_end.wav: {e}")
 
+
 def tela_de_menu(tela, assets):
-    musica_menu() # Toca a música do menu ao iniciar
+    musica_menu()
     while True:
         tela.fill(PRETO)
         exibir_mensagem(tela, "SNAKE GAME", VERDE, -80, assets, fonte_tipo='titulo')
@@ -44,8 +43,9 @@ def tela_de_menu(tela, assets):
                 pontuacao = rodar_jogo(tela, assets)
                 tela_de_game_over(tela, pontuacao, assets)
 
+
 def tela_de_game_over(tela, pontuacao_final, assets):
-    pygame.mixer.music.stop() # Para a música do jogo
+    pygame.mixer.music.stop()
     musica_ending()
     pygame.mixer.music.set_volume(0.3)
     while True:
@@ -60,7 +60,6 @@ def tela_de_game_over(tela, pontuacao_final, assets):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
-                # Ao jogar de novo, ele roda o jogo e atualiza a tela de game over com a nova pontuação
                 nova_pontuacao = rodar_jogo(tela, assets)
                 pontuacao_final = nova_pontuacao
                 pygame.mixer.music.stop()
